f8a4b855e04feb0b8562b62ffa2d63ee42c3bd0f
Enoch <ec965@github.com>
2021-01-07 11:44:21 -0800

    reorganizing

---
+++ build/CMakeFiles/esp-temp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esp-temp.dir/main.cpp.o"
+  "esp-temp"
+  "esp-temp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esp-temp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/esp-temp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for esp-temp.
+# This may be replaced when dependencies are built.
